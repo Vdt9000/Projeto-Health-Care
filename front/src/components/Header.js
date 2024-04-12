049c9c7d4b4c43c95a0883375c8590775b195814
Beatriz Omena <beatrizomena18@gmail.com>
2024-04-11 21:48:06 -0300

    entrega02(front):atualiza interface principal

---
+++ front/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Bem vindo ao Health Care</h1>
+    
+      <nav>
+        <ul>
+          <li><a href="#">inicio</a></li>
+          <li><a href="#">Ajuda</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ front/src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <h2>Bem vindo ao Health Care!</h2>
+      <p>Este é o conteúdo principal da página.</p>
+  
+    </main>
+  );
+}
+
+export default MainContent;
