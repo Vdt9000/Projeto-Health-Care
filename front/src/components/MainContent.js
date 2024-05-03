fcceecce553d44cf2048823db6267f845cc0529c
Beatriz Fernanda da Silva Omena - 823110650 <823110650@ulife.com.br>
2024-05-02 21:51:27 -0300

    Mudanças na interface

--- front/src/components/MainContent.js
+++ front/src/components/MainContent.js
@@ -2,32 +2,63 @@ import React from 'react';
 import './Bordas.css';
 import './Chat.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'primeflex/primeflex.min.css'
+import 'primeicons/primeicons.css'
+
+import 'primereact/resources/themes/lara-dark-amber/theme.css'
 
 
-function MainContent() {
-  return (
-    <main>
-      <ul id="myList">
-    <li><i class="fa-solid fa-house"></i></li> 
-      <li><i class="fa-solid fa-comment"></i></li>
-         <li><i class="fa-solid fa-heart-pulse"></i></li>
-         <li><i class="fa-solid fa-user-nurse"></i></li>
-         <li><i class="fa-solid fa-gear"></i></li>
-      </ul>
-      <ul id="usuario">
-        <li><i class="fa-solid fa-user"></i></li>
-      </ul>
-      <ul id="myList">
-   
-  </ul>
-  <div id="usuario-container">
-    <ul id="usuario">
-     
-    </ul>
-  </div>
-      
-          </main>
-  );
+import { useState } from 'react';
+import { Dock } from 'primereact/dock';
+import { RadioButton } from 'primereact/radiobutton';
+
+
+export default function BasicDemo() {
+    const [position, setPosition] = useState('left');    
+    const items = [
+        {
+            label: 'Photos',
+            icon: () => <img alt="Home" src="https://cdn-icons-png.flaticon.com/512/4084/4084351.png" width="100%" />,
+        },
+        {
+            label: 'Finder',
+            icon: () => <img alt="Config" src="https://cdn-icons-png.flaticon.com/512/3686/3686811.png" width="100%" />,
+        },
+        {
+            label: 'App Store',
+            icon: () => <img alt="Chat" src="https://cdn-icons-png.flaticon.com/512/5962/5962463.png" width="100%" />,
+        },
+        
+        {
+            label: 'Trash',
+            icon: () => <img alt="Lixo" src="https://primefaces.org/cdn/primereact/images/dock/trash.png" width="100%" />,
+        }
+    ];
+
+    const positions = [
+       
+    ];
+
+    return (
+        <div className="card dock-demo">
+            <div className="flex flex-wrap gap-3 mb-5">
+                {positions.map((option) => {
+                    const { value, label } = option;
+
+                    return (
+                        <div className="flex align-items-center" key={label}>
+                            <RadioButton value={label} onChange={() => setPosition(option.value)} checked={position === value} />
+                            <label htmlFor={label} className="ml-2">
+                                {label}
+                            </label>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="dock-window" style={{ backgroundImage: 'url(https://primefaces.org/cdn/primereact/images/dock/window.jpg)' }}>
+                <Dock model={items} position={position} />
+            </div>
+        </div>
+    )
 }
-
-export default MainContent;
+        
