d5eb7a9d0c74fb71c2314819546adb2b5adedf9a
Victor <victordaquino900@gmail.com>
2024-05-15 19:47:48 -0300

    .env

--- back/app.js
+++ back/app.js
@@ -1,11 +1,50 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.get('/hello-world', (req, res) => {
-  res.send('Hello, world!');
+
+app.get("/consultar", (req, res) => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "seu_usuario",
+    password: "sua_senha",
+    database: "sua_base_de_dados",
+  });
+  
+  connection.query("select * from tb_logs", (err, results, fields) => {
+    console.log(results);
+    console.log(fields);
+    res.json(results);
+  });
 });
 
+
+
+app.get("/logs", (req, res) => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "seu_usuario",
+    password: "sua_senha",
+    database: "sua_base_de_dados",
+  });
+  
+  connection.query("select * from tb_logs", (err, results, fields) => {
+    console.log(results);
+    console.log(fields);
+    res.json(results);
+  });
+});
+
+
+
+app.get("/hello-world", (req, res) => {
+  res.send("Hello, world!");
+});
+
+
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Serviço em execução na porta ${3000}`);
 });
