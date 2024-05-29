d09a5582f68e677db7e29a8036d726fc4e896ea4
Beatriz Fernanda da Silva Omena - 823110650 <823110650@ulife.com.br>
2024-05-29 20:43:01 -0300

    Interface changes

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import React from 'react';
 import Header from './components/Header';
-import MainContent from './components/MainContent';
+import Bio from './components/Bio'
 import HealthChat from './components/HealthChat';
+import Salvo from './components/Salvo';
+
+
 
 
 function App() {
   return (
     <div>
       <Header />
-      <MainContent />
+      <Bio/>
       <HealthChat />
+      <Salvo/>
+ 
       
 
      
--- front/src/components/HealthChat.js
+++ front/src/components/HealthChat.js
@@ -1,69 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
 import axios from 'axios';
+import { Dock } from 'primereact/dock';
 
 function HealthChat() {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [position] = useState('left');
 
   const handleMessageSend = () => {
     if (inputValue.trim() !== '') {
-      console.log("Clicou no botão");
       axios.post("http://localhost:3001/pergunte-ao-chatgpt", {prompt: inputValue}).then(x => {
-            setMessages([...messages, { sender: 'bot', text: x.data.completion }]);
-    });
-      console.log("Foi");
-      setMessages([...messages, { sender: 'user', text: inputValue }]);
-      handleChatbotResponse(inputValue); 
+        setMessages(prevMessages => [...prevMessages, { sender: 'bot', text: x.data.completion }]);
+      });
+      setMessages(prevMessages => [...prevMessages, { sender: 'user', text: inputValue }]);
       setInputValue('');
     }
   };
 
- 
-  const handleChatbotResponse = (input) => {
-    // let response = '';
+  const clearChat = () => {
+    setMessages([]);
+  };
 
-    
-    // switch (input.toLowerCase()) {
-    //   case 'dor de cabeça':
-    //     response = 'Sinto muito ouvir isso! Dor de cabeça pode ser realmente desagradável. Você já tentou descansar um pouco em um lugar silencioso e escuro? Às vezes, isso pode ajudar a aliviar um pouco. Também é importante manter-se hidratado e talvez considerar tomar um analgésico suave, se não houver contraindicações para você. Se a dor persistir ou piorar, é sempre uma boa ideia consultar um médico.';
-    //     break;
-    //   case 'garganta ruim':
-    //     response = 'Parece que você não está se sentindo muito bem. Uma garganta irritada ou dolorida pode ser bastante desconfortável. Aqui estão algumas coisas que podem ajudar 1-Gargarejo com água salgada morna: Isso pode ajudar a aliviar a dor e reduzir a inflamação.2-Beba líquidos quentes: Chás de ervas, caldo de galinha ou apenas água quente com mel e limão podem ser reconfortantes para a garganta. 3-Evite irritantes: Tente não fumar, evite fumaça de cigarro e poluentes do ar que possam irritar ainda mais a garganta.4- Mantenha-se hidratado: Beber muitos líquidos pode ajudar a manter a garganta úmida e acelerar o processo de cura. Se a dor de garganta persistir por mais de alguns dias, piorar significativamente ou vier acompanhada de outros sintomas preocupantes, como febre alta, dificuldade para engolir ou falta de ar, é importante procurar orientação médica. Espero que você se sinta melhor em breve!';
-    //     break;
-    //   case 'insônia':
-    //     response = 'A insônia pode ser muito desafiadora, e sei que pode afetar sua qualidade de vida, pode ser útil consultar um médico para descartar qualquer condição médica subjacente e discutir outras opções de tratamento, como terapia cognitivo-comportamental para insônia (TCC-I) ou medicamentos prescritos.';
-    //     break;
-    //   default:
-    //     response = 'Desculpe, não entendi. Como posso ajudá-lo?';
-    //     break;
-    // }
+  const items = [
+    {
+      label: 'Photos',
+      icon: () => <img alt="Home" src="https://cdn-icons-png.flaticon.com/256/4022/4022659.png" width="100%" onClick={clearChat} />,
+    },
+    {
+      label: 'Trash',
+      icon: () => <img alt="Lixo" src="https://cdn-icons-png.flaticon.com/512/8258/8258343.png" width="100%" onClick={clearChat} />,
+    }
+  ];
 
-    // setMessages([...messages, { sender: 'bot', text: response }]);
+  useEffect(() => {
+    const chatBox = document.querySelector(".chat-box");
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }, [messages]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    handleMessageSend();
   };
 
   return (
-    <div className="chat-container">
-      <div className="chat-box">
-        {messages.map((message, index) => (
-          <div key={index} className={`message ${message.sender}`}>
-            {message.text}
-          </div>
-        ))}
+    <div className="card dock-demo">
+      <div className="flex flex-wrap gap-3 mb-5">
+        <div className="flex align-items-center"></div>
       </div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Escreva brevemente o que esta sentindo (ex: dor de cabeça)"
-        className="input-field"
-      />
-      <button onClick={handleMessageSend} className="send-button">
-        Enviar
-      </button>
+      <div className="dock-window" style={{ backgroundImage: 'url(https://primefaces.org/cdn/primereact/images/dock/window.jpg)' }}>
+        <Dock model={items} position={position} />
+      </div>
+      <div className="chat-container">
+        <div className="chat-box">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.sender}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Escreva brevemente o que está sentindo (ex: dor de cabeça)"
+            className="input-field"
+          />
+          <button type="submit" className="send-button">
+            Enviar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default HealthChat;
+
 
